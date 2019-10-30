a44baec85537a4e101e7403774b7fd858d8fc9dc
Maxime RENAUD <mre@cnam-lyon.peda>
2019-10-30 11:58:36 +0100

    second push 30102019, Modifications templates

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +15,14 @@ class UserType extends AbstractType
     {
         $builder
             ->add('login')
-            ->add('roles')
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe est invalide',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Repeter le mot de passe'],
+            ]);
         ;
     }
 
